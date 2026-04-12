4500bcd9541c5aba3db5891383e8f955a8e179d4
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2026-04-12 15:29:17 +0300

    autocomplete fix for non-root access

--- mm-autocomplete.sh
+++ mm-autocomplete.sh
@@ -1,8 +1,9 @@
 _mm_completion() {
+	local DIR=~/mm
 	local names
-	names+=" $(ls -1 /root/mm/var/machines)"
-	names+=" $(ls -1 /root/mm/var/deploys)"
-	names+=" $(ls /root/mm/cmd)"
+	names+=" $(ls -1 $DIR/var/machines)"
+	names+=" $(ls -1 $DIR/var/deploys)"
+	names+=" $(ls $DIR/cmd)"
 	COMPREPLY=($(compgen -W "$names" -- "$2"))
 }
 complete -F _mm_completion mm
